45d3112c429ae67be50583e42e3fa15d4142775a
Roman <romalupan89@gmail.com>
2023-08-30 21:02:08 +0300

    blank template

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// import express from 'express';
+// import cors from 'cors'
+// import {config} from 'dotenv';
+// import mongoose from 'mongoose';
+
+// * Uncomment to initialize the server
+//  const app = express();
+// * Parse the incoming requests with JSON payloads
+// app.use(express.json());
+// * Using cors to get rid of the cors origin error in the request
+/* 
+    app.use(
+        cors({
+            origin: '*',
+        })
+    );
+*/
+
+// * Listen to the port of the server
+// app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
+
+// * Uncomment the line bellow ans set the desired port number
+// const PORT =
